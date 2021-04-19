0b895c302f054db7b14bc90144673cbaacbb3982
Maxence Lagalle <maxence.lagalle@icloud.com>
2021-04-19 21:58:33 +0200

    Begin DataFrame : getitem with overflow, begin setitem

--- DataModel/dataframe.py
+++ DataModel/dataframe.py
@@ -14,36 +14,75 @@ class DataFrame:
         if isinstance(item, str):
             if item in self.__columns:
                 return self.__data[item]
+            else:
+                raise ValueError
         elif isinstance(item, int):
-            return self.__data[self.__columns[item]]
+            if 0 <= item < len(self.__columns):
+                return self.__data[self.__columns[item]]
+            else:
+                raise OverflowError
         elif isinstance(item, tuple):
             if len(item) == 1:
-                return self.__data[self.__columns[item[0]]]
+                if 0 <= item[0] < len(self.__columns):
+                    return self.__data[self.__columns[item[0]]]
+                else:
+                    raise OverflowError
             elif len(item) == 2:
                 if (item[0] is not None) and (item[1] is not None):
                     if isinstance(item[1], int):
                         if isinstance(item[0], int):
-                            return self.__data[self.__columns[item[0]]][item[1]]
+                            if (0 <= item[0] < len(self.__columns)) and (0 <= item[1] < len(self.__data[self.__columns[0]])):
+                                return self.__data[self.__columns[item[0]]][item[1]]
+                            else:
+                                raise OverflowError
                         elif isinstance(item[0], str):
-                            return self.__data[item[0]][item[1]]
+                            if item[0] in self.__columns and (0 <= item[1] < len(self.__data[self.__columns[0]])):
+                                return self.__data[item[0]][item[1]]
+                            else:
+                                raise OverflowError
                         else:
                             raise TypeError
                     else:
                         raise TypeError
                 elif (item[0] is not None) and (item[1] is None):
                     if isinstance(item[0], int):
-                        return self.__data[self.__columns[item[0]]]
+                        if (0 <= item[0] < len(self.__columns)):
+                            return self.__data[self.__columns[item[0]]]
+                        else:
+                            raise OverflowError
                     elif isinstance(item[0], str):
-                        return self.__data[item[0]]
+                        if item[0] in self.__columns:
+                            return self.__data[item[0]]
+                        else:
+                            raise OverflowError
                     else:
                         raise TypeError
                 elif (item[0] is None) and (item[1] is not None):
-                    row = []
-                    for var in self.__columns:
-                        row.append(self.__data[var][item[1]])
-                    return row
+                    if 0 <= item[1] < len(self.__data[self.__columns[0]]):
+                        row = []
+                        for var in self.__columns:
+                            row.append(self.__data[var][item[1]])
+                        return row
+                    else:
+                        return OverflowError
                 else:
                     return ValueError
             else:
                 raise ValueError
 
+    def __setitem__(self, key, value):
+        if isinstance(key,str):
+            if isinstance(value,list):
+                if key in self.__columns:
+                    if len(self.__data[key]) == len(value):
+                        self.__data[key] = value
+                    else:
+                        raise OverflowError
+                else:
+                    raise ValueError
+            else:
+                raise TypeError
+
+test = {"entiers":[1,2,3],"carres":[1,4,9],"cubes":[1,8,27]}
+df = DataFrame(test)
+print(df['cubes'])
