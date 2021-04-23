c803fa2308ee9d284e64301230abd01da2b7669a
Maxence Lagalle <maxence.lagalle@icloud.com>
2021-04-23 12:25:25 +0200

    import.py : PEP

--- IO/import.py
+++ IO/import.py
@@ -2,15 +2,16 @@ import csv
 import json
 from DataModel.dataframe import DataFrame
 
+
 class Import:
     @classmethod
-    def importCSV(cls, path, headers=True, delimiter=";", encoding='ISO-8859-1'):
+    def import_csv(cls, path, headers=True, delimiter=";", encoding='ISO-8859-1'):
         df = DataFrame()
-        with open(path, encoding=encoding) as csvfile:
-            reader = csv.reader(csvfile, delimiter=delimiter)
-            firstRow = True
+        with open(path, encoding=encoding) as csv_file:
+            reader = csv.reader(csv_file, delimiter=delimiter)
+            first_row = True
             for row in reader:
-                if firstRow:
+                if first_row:
                     if headers:
                         for var in row:
                             df.add_column(var)
@@ -18,13 +19,13 @@ class Import:
                         for i in range(len(row)):
                             name = "Var" + str(i)
                             df.add_column(name)
-                    firstRow = False
+                    first_row = False
                 else:
                     df.add_row(row)
         return df
 
     @classmethod
-    def importJSON(cls, path, root=None):
+    def import_json(cls, path, root=None):
         with open(path) as jsonfile:
             data = json.load(jsonfile)
             roots = list(data.keys())
@@ -33,9 +34,9 @@ class Import:
             elif root not in roots:
                 raise KeyError
         table = data[root]
-        vars = list(table[0].keys())
+        table_vars = list(table[0].keys())
         df = DataFrame()
-        for var in vars:
+        for var in table_vars:
             df.add_column(var)
         for row in table:
             df.add_row(list(row.values()))
