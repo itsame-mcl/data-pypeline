0fa58e48326a2b918887936b6d58c09d4c38245b
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-04-29 22:48:34 +0200

    Ungroup transformation

---
+++ Transform/ungroup.py
@@ -0,0 +1,10 @@
+from Pipeline.pipelineable import Pipelineable
+from copy import deepcopy
+
+
+class Ungroup(Pipelineable):
+    def apply(self, df):
+        result = deepcopy(df)
+        for var in df.groups_vars:
+            result.del_group(var)
+        return result
