7da77b87250b73dc88e9a4133346e530b81aa45a
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-04-30 11:28:58 +0200

    Much more user friendly Filter

--- Transform/filter.py
+++ Transform/filter.py
@@ -1,21 +1,30 @@
-from Pipeline import OnVars
+from Pipeline import Pipelineable
 from DataModel import DataFrame
-from Transform.mutate import Mutate
 from Transform.groupby import GroupBy
 
 
-class Filter(OnVars):
-    def __init__(self, fun_criteria, *on_vars):
-        super().__init__(*on_vars)
-        self.__criteria = fun_criteria
+class Filter(Pipelineable):
+    def __init__(self, **criteria):
+        if any(not isinstance(criterion, str) for criterion in list(criteria.values())):
+            raise TypeError
+        self.__criteria = criteria
 
     def apply(self, df):
+        vars_with_criterion = list(self.__criteria.keys())
+        if any(var not in df.vars for var in vars_with_criterion):
+            raise KeyError
         result = DataFrame()
         for var in df.vars:
             result.add_column(var)
-        keep = Mutate("Keep", self.__criteria, *self.vars).apply(df)["Keep"]
-        for row, i in zip(df, range(len(df))):
-            if keep[i]:
-                result.add_row(row)
+        for i in range(len(df)):
+            add_row = True
+            for var in vars_with_criterion:
+                test_result = eval("df['" + str(var) + "'," + str(i) + "] " + str(self.__criteria[var]))
+                if isinstance(test_result, bool):
+                    add_row *= test_result
+                else:
+                    raise TypeError
+            if add_row:
+                result.add_row(df[None, i])
         result = GroupBy(*df.groups_vars).apply(result)
         return result
