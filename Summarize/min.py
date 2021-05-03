42bd9e08d48836da6a59d0d87c6220d740e146c6
Alanna GENIN <alannagenin@gmail.com>
2021-05-03 10:26:41 +0200

    min.py

---
+++ Summarize/min.py
@@ -0,0 +1,10 @@
+from summarizeongroups import SummarizeOnGroups
+
+
+class Min(SummarizeOnGroups):
+    def _operation(self, col):
+        val_min = None
+        for val in col:
+            if (val_min is None) or (val < val_min):
+                val_min = val
+        return {"Min": val_min}
