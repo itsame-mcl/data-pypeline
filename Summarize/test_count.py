a68c46a64d4f08a0c3a691009008d755f884ecba
Alanna GENIN <alannagenin@gmail.com>
2021-05-10 12:46:51 +0200

    test_count.py : management NaN and NA

--- Summarize/test_count.py
+++ Summarize/test_count.py
@@ -9,7 +9,8 @@ class TestCount(unittest.TestCase):
     def setUp(self):
         self.df = DataFrame({"Cat": ["A", "A", "A", "B", "B", "B", "C", "C"],
                              "Var1": [348, 402, 397, 380, 589, 520, 620, 289],
-                             "Var2": [74, 81, 85, 71, 102, 99, 101, 76]})
+                             "Var2": [74, 81, 85, 71, 102, 99, 101, 76],
+                             "Var3": [74, None, 85, 71, "NaN", 99, 101, 76]})
 
     def test_count_var1(self):
         self.assertEqual(Count("Var1").apply(self.df)["Var1_Count"], [len(self.df["Var1"])])
@@ -27,6 +28,11 @@ class TestCount(unittest.TestCase):
         self.assertEqual(Pipeline(GroupBy("Cat"), Count("Var2")).apply(self.df)["Var2_Count"][1], 3)
         self.assertEqual(Pipeline(GroupBy("Cat"), Count("Var2")).apply(self.df)["Var2_Count"][2], 2)
 
+    def test_count_var3_with_nan_na(self):
+        self.assertEqual(Count("Var3", delete_nan=False, delete_na=False).apply(self.df)["Var3_Count"], [len(self.df["Var3"])])
+
+    def test_count_var3_without_nan_na(self):
+        self.assertEqual(Count("Var3").apply(self.df)["Var3_Count"], [len(self.df["Var3"])-2])
 
 if __name__ == '__main__':
     unittest.main()
