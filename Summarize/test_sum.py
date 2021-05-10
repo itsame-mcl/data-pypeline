62883da4ff2921f04e03fc1828fcd67a3ef14068
Alanna GENIN <alannagenin@gmail.com>
2021-05-10 10:51:35 +0200

    test_sum.py : test on the whole dataset and with GroupBy

---
+++ Summarize/test_sum.py
@@ -0,0 +1,32 @@
+import unittest
+from DataModel import DataFrame
+from Summarize import Sum
+from Transform import GroupBy
+from Pipeline import Pipeline
+
+
+class TestSum(unittest.TestCase):
+    def setUp(self):
+        self.df = DataFrame({"Id": ["01", "02", "03", "04", "05", "06", "07", "08"],
+                             "Cat": ["A", "A", "A", "B", "B", "B", "C", "C"],
+                             "Var1": [348, 402, 397, 380, 589, 520, 620, 289],
+                             "Var2": [74, 81, 85, 71, 102, 99, 101, 76]})
+
+    def test_sum_var1(self):
+        self.assertEqual(Sum("Var1").apply(self.df)["Var1_Sum"], [3545])
+
+    def test_sum_var1_group_by(self):
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var1")).apply(self.df)["Var1_Sum"][0], 1147)
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var1")).apply(self.df)["Var1_Sum"][1], 1489)
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var1")).apply(self.df)["Var1_Sum"][2], 909)
+
+    def test_sum_var2(self):
+        self.assertEqual(Sum("Var2").apply(self.df)["Var2_Sum"], [689])
+
+    def test_sum_var2_group_by(self):
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var2")).apply(self.df)["Var2_Sum"][0], 240)
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var2")).apply(self.df)["Var2_Sum"][1], 272)
+        self.assertEqual(Pipeline(GroupBy("Cat"), Sum("Var2")).apply(self.df)["Var2_Sum"][2], 177)
+
+if __name__ == "__main__":
+    unittest.main()
