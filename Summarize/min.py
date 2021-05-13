90914d634ebc14888d7d7c833ad7e897d82180ca
Alanna GENIN <alannagenin@gmail.com>
2021-05-13 15:03:52 +0200

    min.py : class docstring

--- Summarize/min.py
+++ Summarize/min.py
@@ -2,6 +2,17 @@ from Summarize import SummarizeOnGroups
 
 
 class Min(SummarizeOnGroups):
+    """
+        Calculates the minimum of a column (of a DataFrame).
+
+        ...
+
+        Methods
+        -------
+        _operation(col) : dict
+            Calculates the number minimum on the col object, assuming this DataFrame represents a single group.
+            The output variable is called "Variable_Min" (if col = Variable).
+    """
     def _operation(self, col):
         val_min = None
         for val in col:
