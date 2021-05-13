85e21e0b9a3c7aa52e2585a0dc53fa04d24373a7
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-05-13 11:26:27 +0200

    pipeline.py : class docstring

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -3,6 +3,33 @@ from copy import deepcopy
 
 
 class Pipeline(Pipelineable):
+    """
+    A Pipeline of pipelineable operations to apply on a DataFrame.
+
+    Is itself a pipelinable operation.
+
+    ...
+
+    Attributes
+    ----------
+    self.__operations : list
+        List of pipelineable operations to apply on a DataFrame
+
+    Methods
+    -------
+    __init__(*operations)
+        Create a Pipeline with the specified operations
+    add_operations(*operations)
+        Add new operations to the Pipeline after it's creation
+    operations : list
+        Returns the list of currently specified operations
+    del_operations(*operations)
+        Remove operations to the Pipeline after it's creation
+    apply(df) : DataFrame
+        Apply the operations of the PipeLine to the DataFrame df and returns the final DataFrame
+    _secure_add_operations(operations):
+        Handles the pipelineable checking when adding operations
+    """
     def __init__(self, *operations):
         self.__operations = []
         self.__secure_add_operations(operations)
