011ee598ebba8f17fce0426837d6cc692fc6aa2e
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-05-13 10:55:43 +0200

    exportmapfrmetro.py : class docstring

--- IO/exportmapfrmetro.py
+++ IO/exportmapfrmetro.py
@@ -7,6 +7,41 @@ from Pipeline import Pipelineable
 
 
 class ExportMapFRMetro(Pipelineable):
+    """
+    Exports a DataFrame as a Metropolitan France map.
+
+    ...
+
+    Attributes
+    ----------
+    self.__path : str
+        Absolute or relative path of the map image file to generate
+    self.__json_path : str
+        Relative path of the GeoJSON file with the correct administrative division
+    self.__var_geocode : str
+        Name of the variable with the geographic codes in the DataFrame
+    self.__var_tomap : str
+        Name of the vairable with the values to display in the map
+    self.__title : str
+        Title of the map
+    self.__color_scale : str
+        Name of the matplotlib's color scale to use
+    self.__display_lables : bool
+        Displays the geographic codes on the map if True
+    self.__display_scale : bool
+        Displays the colors scale right to the map if True
+
+    Methods
+    -------
+    __init__(path, type_geo, var_geocode, var_tomap, title=None, color_scale='viridis',
+                 display_labels=True, display_scale=True)
+        Create an ExportMapFRMetro pipelinable object and define the specifications of the future map.
+        type_geo must be set to "dep" or "departement" for a map at departmental scale or to "reg" or
+        "region" for a map at regional scale.
+
+    apply(df)
+        Takes the DataFrame df and exports it as map image file
+    """
     def __init__(self, path, type_geo, var_geocode, var_tomap, title=None, color_scale='viridis',
                  display_labels=True, display_scale=True):
         self.__path = path
