f17d3a9f8a40a4d3e2630b7f276ea66332d990bd
Alanna GENIN <alannagenin@gmail.com>
2021-05-13 15:00:26 +0200

    max.py : class docstring

--- Summarize/max.py
+++ Summarize/max.py
@@ -2,6 +2,17 @@ from Summarize import SummarizeOnGroups
 
 
 class Max(SummarizeOnGroups):
+    """
+        Calculates the maximum of a column (of a DataFrame).
+
+        ...
+
+        Methods
+        -------
+        _operation(col) : dict
+            Calculates the number maximum on the col object, assuming this DataFrame represents a single group.
+            The output variable is called "Variable_Max" (if col = Variable).
+    """
     def _operation(self, col):
         val_max = None
         for val in col:
