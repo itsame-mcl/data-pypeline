e045a5711cc0d5b0177b7ad759a930192389240e
Maxence Lagalle <maxence.lagalle@outlook.fr>
2021-05-13 10:38:34 +0200

    exportcsv.py : class docstring

--- IO/exportcsv.py
+++ IO/exportcsv.py
@@ -4,6 +4,29 @@ import csv
 
 
 class ExportCSV(Pipelineable):
+    """
+    Exports a DataFrame as CSV file.
+
+    ...
+
+    Attributes
+    ----------
+    self.__path : str
+        Absolute or relative path of the CSV file to generate
+    self.__headers : bool
+        Indicates if the headers of the DataFrame will be exported or not
+    self.__delimiter : str
+        Specify the delimiter character on the CSV file
+    self.__encoding : str
+        Specify the encoding of the CSV file
+
+    Methods
+    -------
+    __init__(path, headers=True, delimiter=";", encoding='ISO-8859-1')
+        Create an ExportCSV pipelinable object and define the specifications of the future CSV file.
+    apply(df)
+        Takes the DataFrame df and exports it as CSV file
+    """
     def __init__(self, path, headers=True, delimiter=";", encoding='ISO-8859-1'):
         self.__path = path
         self.__headers = headers
