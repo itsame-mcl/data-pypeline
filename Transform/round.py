9c1fe9e3ccd5ec482b2c28793820eac3b4194d70
Alanna GENIN <alannagenin@gmail.com>
2021-05-19 16:50:02 +0200

    Create transformation round

---
+++ Transform/round.py
@@ -0,0 +1,30 @@
+from Pipeline import OnVars
+from copy import deepcopy
+
+
+class Round(OnVars):
+    """
+    Round numerical data with the desired number of commas.
+
+    ...
+
+    Attributes
+    ----------
+    self.__precision : int
+        desired number of decimal places
+
+    Methods
+    -------
+    apply(df) : DataFrame
+        Rounds each variable of a DataFrame df to a given precision
+
+    """
+    def __init__(self, *on_vars, precision=2):
+        super().__init__(*on_vars)
+        self.__precision = precision
+
+    def apply(self, df):
+        result = deepcopy(df)
+        for var in self.vars:
+            result[var] = [float(int((val * 10 ** self.__precision))) / (10 ** self.__precision) for val in df[var]]
+        return result
