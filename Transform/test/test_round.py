c6eb48dd7176dc333f4081a3c70c91397e02993f
Alanna GENIN <alannagenin@gmail.com>
2021-05-19 18:44:55 +0200

    Unittest round with data text as numeric

--- Transform/test/test_round.py
+++ Transform/test/test_round.py
@@ -1,6 +1,7 @@
 import unittest
 from DataModel import DataFrame
-from Transform import Round
+from Transform import AsNumeric, Round
+from Pipeline import Pipeline
 
 
 class TestRound(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestRound(unittest.TestCase):
                              'Date': ["2021-03-01", "2021-03-02", "2021-03-03",
                                       "2021-03-01", "2021-03-02", "2021-03-03", "2021-03-04",
                                       "2021-03-01", "2021-03-02", "2021-03-03"],
-                             'Var1': [10.12, 14.2634, 13.9122, 22.74, 28.09, 23.1, 30, 6.061, 8.57, 9.532],
+                             'Var1': [10.123, 14.2634, 13.9122, 22.74, 28.09, 23.1, 30, 6.06, 8.57, 9.532],
                              'Var2': [250.1823, 245.682109, 209.0111111111, 360.873421, 328.09534163, 359.07426, 372.07227173, 74, 78, 80],
                              'VarNone': [87.0912, 99.102, None, 120, 128.88, None, 99.64, None, None, None],
                              'VarMixed': [-3, 2, "Null", 0, None, "Null", 5, None, "Null", "Null"],
@@ -18,7 +19,7 @@ class TestRound(unittest.TestCase):
     def test_round(self):
         arrondi = Round('Var1')
         result = arrondi.apply(self.df)
-        self.assertEqual([10.11, 14.26, 13.91, 22.74, 28.09, 23.1, 30, 6.06, 8.57, 9.53], result[2])
+        self.assertEqual([10.12, 14.26, 13.91, 22.74, 28.09, 23.1, 30, 6.06, 8.57, 9.53], result[2])
 
     def test_round_with_precision(self):
         arrondi = Round('Var2', precision=3)
@@ -31,6 +32,11 @@ class TestRound(unittest.TestCase):
         result = arrondi.apply(self.df)
         self.assertEqual([87.09, 99.10, None, 120, 128.88, None, 99.64, None, None, None], result[4])
 
+    def test_round_as_numeric(self):
+        convertir = AsNumeric('VarTextNum')
+        arrondi = Round('VarTextNum', precision=0)
+        result = Pipeline(convertir, arrondi).apply(self.df)
+        self.assertEqual([5, 8, -1, 0, 7, 11, -8, 5, -4, 9], result[6])
 
 if __name__ == '__main__':
     unittest.main()
